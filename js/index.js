abeb60ab4a394b342887967a5d91b1d7dc50a213
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-03-16 00:03:32 +0200

    Solve Problem 73

--- js/index.js
+++ js/index.js
@@ -688,10 +688,23 @@
 //   return yourPoints > classPointAvg;
 // }
 //& 72- Reversed Words
-function reverseWords(str) {
-  return str
-    .split(" ")
-    .map((item) => item.split("").reverse().join(""))
-    .join(" ");
+// function reverseWords(str) {
+//   return str
+//     .split(" ")
+//     .map((item) => item.split("").reverse().join(""))
+//     .join(" ");
+// }
+// console.log(reverseWords("double  spaces"));
+//& 73- Calculate BMI
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  if (bmi <= 18.5) {
+    return "Underweight";
+  } else if (bmi <= 25.0) {
+    return "Normal";
+  } else if (bmi <= 30.0) {
+    return "Overweight";
+  } else if (bmi > 30) {
+    return "Obese";
+  }
 }
-console.log(reverseWords("double  spaces"));
