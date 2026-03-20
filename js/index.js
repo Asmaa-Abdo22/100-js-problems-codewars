bc8021d1b8e5df5f2cfe89e7fe04875da577c56e
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-03-20 23:41:01 +0200

    Solve Problem 74

--- js/index.js
+++ js/index.js
@@ -696,15 +696,19 @@
 // }
 // console.log(reverseWords("double  spaces"));
 //& 73- Calculate BMI
-function bmi(weight, height) {
-  let bmi = weight / (height * height);
-  if (bmi <= 18.5) {
-    return "Underweight";
-  } else if (bmi <= 25.0) {
-    return "Normal";
-  } else if (bmi <= 30.0) {
-    return "Overweight";
-  } else if (bmi > 30) {
-    return "Obese";
-  }
+// function bmi(weight, height) {
+//   let bmi = weight / (height * height);
+//   if (bmi <= 18.5) {
+//     return "Underweight";
+//   } else if (bmi <= 25.0) {
+//     return "Normal";
+//   } else if (bmi <= 30.0) {
+//     return "Overweight";
+//   } else if (bmi > 30) {
+//     return "Obese";
+//   }
+// }
+//& 74- Find numbers which are divisible by given number
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((item) => item % divisor === 0);
 }
