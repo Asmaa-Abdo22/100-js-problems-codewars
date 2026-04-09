e82466677f0e65949d41b423d42eca1bbca72170
Asmaa-Abdo22 <asmaabdelwadoud194@gmail.com>
2026-04-10 00:03:02 +0200

    Solve problem 87

--- js/index.js
+++ js/index.js
@@ -767,10 +767,18 @@
 //   return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
 // }
 //& 86- Stringy Strings
-function stringy(size) {
-  let result = "";
-  for (let i = 0; i < size; i++) {
-    result += i % 2 == 0 ? "1" : "0";
+// function stringy(size) {
+//   let result = "";
+//   for (let i = 0; i < size; i++) {
+//     result += i % 2 == 0 ? "1" : "0";
+//   }
+//   return result;
+// }
+//& 87- Find Multiples of a Number
+function findMultiples(integer, limit) {
+  let result = [];
+  for (let i = integer; i <= limit; i += integer) {
+    result.push(i);
   }
   return result;
 }
